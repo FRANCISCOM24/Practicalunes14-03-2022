e4489ccb609f475efc3051246f0f5afc6c891e63
Centro de Computo <ccomputo@itvy.edu.mx>
2022-03-14 09:58:33 -0700

    Version 1 |

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+function Componente4()
+{
+  return(
+    alert('Hola mundo')
+  );
+}
+
+function Componente3(props)
+{
+return(
+
+  <h4>{props.parametro1} {props.parametro2}</h4>
+
+);
+
+}
+
+function Componente2()
+{
+return(
+<h3>Jalate perro</h3>
+
+);
+
+}
+
+function Componente1()
+{
+return(
+  <h2>ESTA ES UNA ETIQUETA</h2>
+  );
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola mundo (⊃｡•́‿•̀｡)⊃
+      <Componente1></Componente1>
+      <Componente2></Componente2>
+     <Componente3 parametro1="Jesus" parametro2= "Francisco"></Componente3>
+      <button onClick={Componente4}>Click aqui </button>
+  
+
+    
     </div>
   );
 }
